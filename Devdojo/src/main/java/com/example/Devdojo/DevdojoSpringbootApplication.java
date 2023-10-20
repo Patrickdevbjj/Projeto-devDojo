e98acacbfe8d93f0c243d047e9db38d06674c89f
Patrick Rodrigues <patrickdevbjj6@hotmail.com>
2023-10-20 18:04:45 -0300

    Meu novo projeto

---
+++ Devdojo/src/main/java/com/example/Devdojo/DevdojoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Devdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdojoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdojoSpringbootApplication.class, args);
+	}
+
+}
