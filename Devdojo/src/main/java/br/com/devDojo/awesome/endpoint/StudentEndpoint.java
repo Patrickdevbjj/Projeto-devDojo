883ee162192add2e4c0356fc35bf249863f12658
Patrick Rodrigues <patrickdev6@hotmail.com>
2023-11-29 00:45:30 -0300

    Update with POST method

--- Devdojo/src/main/java/br/com/devDojo/awesome/endpoint/StudentEndpoint.java
+++ Devdojo/src/main/java/br/com/devDojo/awesome/endpoint/StudentEndpoint.java
@@ -6,10 +6,7 @@ import br.com.devDojo.awesome.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("students")
@@ -39,5 +36,9 @@ public class StudentEndpoint {
         }
         return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
-
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student){
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
 }
